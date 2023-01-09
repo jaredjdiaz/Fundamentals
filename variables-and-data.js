928e4fed3118a5e4d4ef4c928422bb87d2c9e121
Jared Diaz <jdiaz97@cnm.edu>
2023-01-09 01:13:57 -0700

    Code from following three videos combined into one project for Phase 2

---
+++ variables-and-data.js
@@ -0,0 +1,36 @@
+/*
+ *Variables, Data types, and Typing
+ */
+let wordData;
+
+wordData = "Word list";
+
+console.log(wordData);
+
+let subTotal = 100;
+let tax = subTotal * 0.07;
+let totalWithTax = tax + subTotal;
+/*
+ *Variables, Data types, and Typing
+ */
+let obj = {
+    key1:"Value 1"
+    key2: 3,
+    obj2: {
+        objKey1 : "value of inner object",
+    },
+};
+
+console.log(obj.obj2.objKey1);
+/*
+ *Variables, Data types, and Typing
+ */
+let add = 1 + 2;
+let sub = 2 - 1;
+let mult = 2*4;
+let division = 4 / 2;
+let mod = 5 % 2;
+
+let string = "String 1 " + "String 2";
+
+console.log(string);
